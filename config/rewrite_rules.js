6297dcece04eca90a819acc28d392c584d818d3b
Kael <i@kael.me>
2013-03-26 19:04:55 +0800

    remote unnecessary files

--- config/rewrite_rules.js
+++
@@ -1,8 +0,0 @@
-
-var rules = [
-	[/\.v\d+\.(png|jpg|gif|css|js|swf)/g,'.$1'], // remove version for all static files
-	[/\.min.*\.(js|css)$/g,'.$1'] // remove min for js and css
-];
-
-
-module.exports = rules;
